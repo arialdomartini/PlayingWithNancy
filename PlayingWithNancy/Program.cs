69d424497647ad7486967dcdd7782bb140160119
Arialdo Martini <arialdomartini@gmail.com>
2015-06-28 19:40:09 +0200

    index shows a string

---
+++ PlayingWithNancy/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Nancy;
+
+namespace PlayingWithNancy
+{
+    public class Program : NancyModule
+    {
+        static void Main(string[] args) {
+            Console.Write("Starting server...");
+            var server = new Nancy.Hosting.Self.NancyHost(new Uri("http://localhost:8282"));
+            server.Start();
+            Console.Read();
+        }
+
+        public Program()
+        {
+            Get["/"] = _ => { return "Nancy says hello!"; };
+        }
+    }
+
+}
