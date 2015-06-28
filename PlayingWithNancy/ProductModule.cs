06c0031b64325d92e525e6b4262177527c038ea6
Arialdo Martini <arialdomartini@gmail.com>
2015-06-28 20:31:25 +0200

    Using a lambda

--- PlayingWithNancy/ProductModule.cs
+++ PlayingWithNancy/ProductModule.cs
@@ -1,13 +1,16 @@
 ﻿using System;
 using Nancy;
+using System.Collections.Generic;
 
 namespace PlayingWithNancy
 {
     public class ProductModule : NancyModule
     {
+        List<string> _products = new List<string>() { "one", "two", "tree"};
+
         public ProductModule() : base("products")
         {
-            Get["list"] = _ => { return "List of values"; };
+            Get["list"] = _ => string.Join(", ", _products);
         }
     }
 }
