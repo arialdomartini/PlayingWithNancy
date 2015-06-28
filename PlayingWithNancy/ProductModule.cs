dcb569394bd335f0188f62c0ad81ae50b475f3e3
Arialdo Martini <arialdomartini@gmail.com>
2015-06-28 20:27:36 +0200

    /products/list using base("/products")

---
+++ PlayingWithNancy/ProductModule.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using Nancy;
+
+namespace PlayingWithNancy
+{
+    public class ProductModule : NancyModule
+    {
+        public ProductModule() : base("products")
+        {
+            Get["list"] = _ => { return "List of values"; };
+        }
+    }
+}
